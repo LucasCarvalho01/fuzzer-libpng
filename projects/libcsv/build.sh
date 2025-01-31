df29bc3bd3b0a2b3ce4123f969f12f2f21739fad
Lucas Carvalho <lucascarvalhopc@gmail.com>
2025-01-31 14:03:27 -0300

    fuzzer funcionando

--- projects/libcsv/build.sh
+++ projects/libcsv/build.sh
@@ -4,7 +4,7 @@ autoreconf -i
 ./configure
 make -j$(nproc)
 
-$CXX $CXXFLAGS -std=c++11 -Iinclude \
+$CXX $CXXFLAGS -std=c++11 -I/src/include \
     $SRC/libcsv_fuzzer.cc \
     -o $OUT/libcsv_fuzzer \
     $LIB_FUZZING_ENGINE .libs/libcsv.a
